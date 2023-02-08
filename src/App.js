9a3d228996a2a141754604f0541986c5a5bd02c5
bekirin50tonu <bgrmz@yandex.com>
2023-02-08 05:57:19 +0300

    new file:   build/asset-manifest.json 	new file:   build/favicon.ico 	new file:   build/index.html 	new file:   build/logo192.png 	new file:   build/logo512.png 	new file:   build/manifest.json 	new file:   build/robots.txt 	new file:   build/static/css/main.4da77a7f.css 	new file:   build/static/css/main.4da77a7f.css.map 	new file:   build/static/js/787.0c3f432a.chunk.js 	new file:   build/static/js/787.0c3f432a.chunk.js.map 	new file:   build/static/js/main.7b116f46.js 	new file:   build/static/js/main.7b116f46.js.LICENSE.txt 	new file:   build/static/js/main.7b116f46.js.map

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [link, UseLink] = useState("");
+  const [links, UseLinks] = useState([
+    "SqHIO2zhxbA",
+    "lxGVhhq2jV0",
+    "Rc5qrxlJZzc",
+    "fx5_pi-1Zqo",
+    "g4QA9Sh_g_8",
+    "XEJM4Hcgd3M",
+    "sd94keSra6A",
+    "fjjsrVl_Lgc",
+    "tUjKXFrsW74",
+    "L0aI7O5KrVU",
+    "njp2SpI39pE",
+    "AEZa1BtbPok",
+  ]);
+
+  const [show, IsShow] = useState(true);
+
+  const idRegex = /youtu(?:.*\/v\/|.*v\=|\.be\/)([A-Za-z0-9_\-]{11})/;
+
+  const resetHandler = () => {
+    UseLink("");
+  };
+
+  const clickHandler = () => {
+    let match = idRegex.exec(link);
+    if (match === null) {
+      return;
+    }
+    let id = match[1];
+    UseLinks([id, ...links]);
+    localStorage.setItem("links", JSON.stringify(links));
+  };
+
+  const onChange = (e) => {
+    console.log("test", link);
+    UseLink(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="button menu"
+        onClick={() => {
+          IsShow(!show);
+        }}
+      >
+        {show === true ? "Küçült" : "Büyült"}
+      </button>
+      <div
+        className="drawer"
+        style={{
+          visibility: show === true ? "visible" : "hidden",
+        }}
+      >
+        <div className="center">
+          Link Giriniz:
+          <input
+            value={link}
+            onInput={(e) => onChange(e)}
+            name="link"
+            type="url"
+          />
+          <button className="button" onClick={() => clickHandler()}>
+            Ekle
+          </button>
+          <button className="button" onClick={() => resetHandler()}>
+            Temizle
+          </button>
+        </div>
+      </div>
+      <div className="container">
+        {links.map((val, index) => (
+          <iframe
+            key={index}
+            width="560"
+            height="315"
+            src={`https://www.youtube.com/embed/${val}?autoplay=1&mute=1&enablejsapi=1`}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowFullScreen
+          ></iframe>
+        ))}
+      </div>
     </div>
   );
 }
